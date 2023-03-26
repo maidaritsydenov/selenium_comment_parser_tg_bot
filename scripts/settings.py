69918b1b5dcf614fb85e76f9f4ebf5fa6829a9df
MaidariTs <tmaidari@mail.ru>
2023-03-26 14:28:35 +0300

    env

--- scripts/settings.py
+++ scripts/settings.py
@@ -5,8 +5,8 @@ HOME_DIR = expanduser("~")
 CURDIR = os.path.abspath(os.curdir)
 
 
-TELEGRAM_TOKEN = '6161827603:AAHIQSOmr5PIcGE4uWqqvxzPmvSN3yAiyoU'
+TELEGRAM_TOKEN = ''
 _PATH_COMMON_PART = f'{HOME_DIR}/._VkChromeClientPipeline/saved'
 PHRASES_FILE_PATH = f'{_PATH_COMMON_PART}/messages.txt'
 SCREEN_FILE_PATH = f'{_PATH_COMMON_PART}/screen.png'
-ALLOWED_USERS = ['maidaritsydenov']
+ALLOWED_USERS = ['']
